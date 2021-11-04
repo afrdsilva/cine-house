93378d3813600ea593ce8a30593db47232caa274
afrdsilva <allyssonfernandes2@gmail.com>
2021-11-04 20:21:02 -0300

    creado arquivo index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+let cinema = ('cine house');
+const catalogo = require('./database/catalogo.json')
+
+
+function adicionarFilme(titulo,duracao,atores,anoLancamento,emCartas){
+    const novoFilme ={
+        codigo: catalogo.lenght,
+        titulo,
+        duracao ,
+        atores,
+        anoLancamento,
+        emCartas
+    }
+    const novoTamanhoDoCatalogo = catalogo.push(novoFilme)
+    console.log('Filme adicionado  com sucesso, catalogo agora tem' +novoTamanhoDoCatalogo +'Filmes')
+}
+
+function verificaDuracao(filme,){
+    return filme.duracao>2
+}
+function listaFilmesLongaDuracao(){
+console.log(catalogo.filter(verificaDuracao))
+
+}
+
+
+
+adicionarFilme('picachu,2.0,[ "allysson"],2020,true')
